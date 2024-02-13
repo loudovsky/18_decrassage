bdd22283a8e0d00f292509a6697ddcb7be2cb9f7
loudovsky <luileptitecossais@gmail.com>
2024-02-13 09:08:23 +0100

    second commit

---
+++ scripts/main.js
@@ -0,0 +1,14 @@
+let myButton = document.querySelector('.button');
+let userInput = document.querySelector('.input');
+let body = document.querySelector('body')
+
+function changeBodyColor() {
+    color = `background-color:${userInput.value}`
+    body.setAttribute("style", color)
+}
+
+myButton.addEventListener('click', function() {
+    changeBodyColor();
+    userInput.value = "";
+})
+
